189d55239b0666d4b12933814073d066aa9b8479
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-08 11:02:20 +0200

    x

--- components/features-section-demo-3.tsx
+++ components/features-section-demo-3.tsx
@@ -181,13 +181,18 @@ export const SkeletonFour = () => {
         <div className="relative flex h-full w-full flex-1 flex-col space-y-2">
           {/* TODO */}
           <IconBrandYoutubeFilled className="absolute inset-0 z-10 m-auto h-20 w-20 text-red-500" />
-          <img
-            src="https://tomaslonnblad.github.io/3.jpg"
-            alt="header"
-            width={800}
-            height={800}
-            className="aspect-square h-full w-full rounded-sm object-cover object-center blur-none transition-all duration-200 group-hover/image:blur-md"
-          />
+      <NoiseBackground
+        containerClassName="w-fit p-2 rounded-full mx-auto"
+        gradientColors={[
+          "rgb(255, 100, 150)",
+          "rgb(100, 150, 255)",
+          "rgb(255, 200, 100)",
+        ]}
+      >
+        <button className="h-full w-full cursor-pointer rounded-full bg-linear-to-r from-neutral-100 via-neutral-100 to-white px-4 py-2 text-black shadow-[0px_2px_0px_0px_var(--color-neutral-50)_inset,0px_0.5px_1px_0px_var(--color-neutral-400)] transition-all duration-100 active:scale-98 dark:from-black dark:via-black dark:to-neutral-900 dark:text-white dark:shadow-[0px_1px_0px_0px_var(--color-neutral-950)_inset,0px_1px_0px_0px_var(--color-neutral-800)]">
+          Start publishing &rarr;
+        </button>
+      </NoiseBackground>
         </div>
       </div>
     </a>
